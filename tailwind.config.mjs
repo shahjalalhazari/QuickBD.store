f405f724d6d837467631e146ca391412741688fc
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2026-02-01 22:03:54 +0400

    Project Created and configured with all necessary data.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,8 +8,22 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "#EE7411",
+        secondary: "#0A0B0D",
+        green: "#38CB89",
+        red: "#ED0006",
+        blue: "#00579F",
+        purple: "#8A38F5",
+        "dark-black": "#3C3C3B",
+        "body-text":"#6C7275",
+        "border-color":"#B8B8B8",
+        "ghost-white": "#F8F8FF",
+        white:"#fff",
+        "light-orange": "#DFDBD7",
+      },
+      fontFamily: {
+        montserrat: ["var(--font-montserrat)"],
+        hind: ["var(--font-hind)"],
       },
     },
   },
