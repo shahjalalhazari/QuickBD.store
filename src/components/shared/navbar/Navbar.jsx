da68bcb35a208f9aae394322ec92c8a458946205
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2026-02-05 00:17:06 +0400

    Make navbar resposive for medium devices.

--- src/components/shared/navbar/Navbar.jsx
+++ src/components/shared/navbar/Navbar.jsx
@@ -1,10 +1,56 @@
-import LargeNavbar from "./LargeNavbar";
+"use client"
+import Link from "next/link";
 import "./navbar.css";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+import { FaBagShopping, FaCircleUser, FaRegCircleUser } from "react-icons/fa6";
+import { FiLogIn, FiShoppingBag } from "react-icons/fi";
 
 const Navbar = () => {
+  const pathname = usePathname();
+
   return (
     <>
-      <LargeNavbar navItems={navItems} />
+      <nav className="quickbd-navbar" role="navigation" aria-label="main navigation">
+
+      {/* NAVBAR LOGO */}
+      <div className="navbar-logo">
+        <Link href="/" className="navbar-item">
+          <Image src="/images/logo/logo.png" alt="Logo" width={160} height={38.27} />
+        </Link>
+      </div>
+
+
+      {/* MAIN NAV ITEMS */}
+      <ul className='navbar-items'>
+
+        {navItems.map((item, index) => (
+          <li key={index} className={`navbar-item quickbd-transition ${pathname === item.path ? 'active-nav' : ''}`}>
+            <Link href={item.path}>{item.title}</Link>
+          </li>
+        ))}
+      </ul>
+
+      {/* NAVBAR ICONS */}
+      <ul className='navbar-icons'>
+        <li className="quickbd-transition">
+          <Link href="/signin">
+            <FaRegCircleUser className="navbar-icon" />
+          </Link>
+        </li>
+        <li className="quickbd-transition">
+          <Link href="/signin">
+            <FiLogIn className="navbar-icon" />
+          </Link>
+        </li>
+        <li className="quickbd-transition">
+          <Link href="/cart" className="cart-icon">
+            <FiShoppingBag className="navbar-icon" />
+            <span className="cart-count">9</span>
+          </Link>
+        </li>
+      </ul>
+    </nav>
     </>
   );
 };
@@ -29,4 +75,4 @@ const navItems = [
     title: "Contact Us",
     path: "/contact-us",
   },
-];
+]
