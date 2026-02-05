67df10ae664a7a7fbbeefef7b98865311d7c8103
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2026-02-05 16:07:18 +0400

    Navbar added for small devices.

--- src/components/shared/navbar/Navbar.jsx
+++ src/components/shared/navbar/Navbar.jsx
@@ -3,46 +3,73 @@ import Link from "next/link";
 import "./navbar.css";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
-import { FaBagShopping, FaCircleUser, FaRegCircleUser } from "react-icons/fa6";
+import { FaRegCircleUser } from "react-icons/fa6";
 import { FiLogIn, FiShoppingBag } from "react-icons/fi";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
   const pathname = usePathname();
 
   return (
     <>
-      <nav className="quickbd-navbar" role="navigation" aria-label="main navigation">
+      <nav role="navigation" aria-label="main navigation">
 
-      {/* NAVBAR LOGO */}
-      <div className="navbar-logo">
-        <Link href="/" className="navbar-item">
-          <Image src="/images/logo/logo.png" alt="Logo" width={160} height={38.27} />
-        </Link>
+      {/* NAVBAR FOR LARGE & MEDIUM DEVICES */}
+      <div className="quickbd-navbar">
+        {/* NAVBAR LOGO */}
+        <div className="navbar-logo">
+          <Link href="/" className="navbar-item">
+            <Image src="/images/logo/logo.png" alt="Logo" width={160} height={38.27} />
+          </Link>
+        </div>
+
+
+        {/* MAIN NAV ITEMS */}
+        <ul className='navbar-items'>
+
+          {navItems.map((item, index) => (
+            <li key={index} className={`navbar-item quickbd-transition ${pathname === item.path ? 'active-nav' : ''}`}>
+              <Link href={item.path}>{item.title}</Link>
+            </li>
+          ))}
+        </ul>
+
+        {/* NAVBAR ICONS */}
+        <ul className='navbar-icons'>
+          <li className="quickbd-transition">
+            <Link href="/signin">
+              <FaRegCircleUser className="navbar-icon" />
+            </Link>
+          </li>
+          <li className="quickbd-transition">
+            <Link href="/signin">
+              <FiLogIn className="navbar-icon" />
+            </Link>
+          </li>
+          <li className="quickbd-transition">
+            <Link href="/cart" className="cart-icon">
+              <FiShoppingBag className="navbar-icon" />
+              <span className="cart-count">9</span>
+            </Link>
+          </li>
+        </ul>
       </div>
 
+      {/* NAVBAR FOR SMALL DEVICES */}
+      <ul className="small-navbar">
+        {/* HAMBURGER ICON */}
+        <li className="hamburger-icon">
+          <GiHamburgerMenu size={28} />
+        </li>
 
-      {/* MAIN NAV ITEMS */}
-      <ul className='navbar-items'>
-
-        {navItems.map((item, index) => (
-          <li key={index} className={`navbar-item quickbd-transition ${pathname === item.path ? 'active-nav' : ''}`}>
-            <Link href={item.path}>{item.title}</Link>
-          </li>
-        ))}
-      </ul>
-
-      {/* NAVBAR ICONS */}
-      <ul className='navbar-icons'>
-        <li className="quickbd-transition">
-          <Link href="/signin">
-            <FaRegCircleUser className="navbar-icon" />
+        {/* NAVBAR LOGO */}
+        <li className="navbar-logo">
+          <Link href="/" className="navbar-item">
+            <Image src="/images/logo/logo.png" alt="Logo" width={110} height={38.27} />
           </Link>
         </li>
-        <li className="quickbd-transition">
-          <Link href="/signin">
-            <FiLogIn className="navbar-icon" />
-          </Link>
-        </li>
+
+        {/* CART ICON */}
         <li className="quickbd-transition">
           <Link href="/cart" className="cart-icon">
             <FiShoppingBag className="navbar-icon" />
