7aa9b3f7635e436e0c56ee7e0de2e6b10a4f1ee4
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2026-02-06 23:40:22 +0400

    BUG FIXED: not showing shape images of Error page.

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -7,37 +7,37 @@ export default function NotFound() {
     <div className="not-found-404">
       {/* BG SHAPES */}
       <Image 
-        src="/shapes/error-page/oval.png" 
+        src="/shapes/error-page/Oval.png" 
         alt="shape" width={195} height={195} 
         className="oval-1"
       />
       <Image 
-        src="/shapes/error-page/objects-01.png" 
+        src="/shapes/error-page/Objects-01.png" 
         alt="shape" width={400} height={250} 
         className="objects-1"
       />
       <Image 
-        src="/shapes/error-page/objects-02.png" 
+        src="/shapes/error-page/Objects-02.png" 
         alt="shape" width={750} height={292} 
         className="objects-2"
       />
       <Image 
-        src="/shapes/error-page/oval-2.png" 
+        src="/shapes/error-page/Oval-2.png" 
         alt="shape" width={59} height={59} 
         className="oval-2  animate-float-y"
       />
       <Image 
-        src="/shapes/error-page/oval-3.png" 
+        src="/shapes/error-page/Oval-3.png" 
         alt="shape" width={63} height={63} 
         className="oval-3  animate-float-x"
       />
 			<Image 
-        src="/shapes/error-page/oval-4.png" 
+        src="/shapes/error-page/Oval-4.png" 
         alt="shape" width={111} height={111} 
         className="oval-4 animate-float-diagonal"
       />
       <Image 
-        src="/shapes/error-page/oval-5.png" 
+        src="/shapes/error-page/Oval-5.png" 
         alt="shape" width={167} height={167} 
         className="oval-5  animate-float-y"
       />
