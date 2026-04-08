e86946aeecfc99911b075f4cad0c531b9191c0de
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2026-04-08 21:32:52 +0400

    Loading spinner made center of page from both side.

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,5 +1,9 @@
 import QuickbdLoading from "@/components/shared/QuickbdLoading";
 
 export default function Loading() {
-  return <QuickbdLoading/>
+  return (
+    <div className="fixed inset-0 flex items-center justify-center z-[1010]">
+      <QuickbdLoading sizes="w-8 h-8 md:w-10 md:h-10"/>
+    </div>
+  )
 }
