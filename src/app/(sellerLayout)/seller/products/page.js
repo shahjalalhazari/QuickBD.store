47722142baa859d895bee87b6371f8957be093cf
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2026-05-14 19:49:59 +0400

    Responsive Stat Cards added in Products Page.

--- src/app/(sellerLayout)/seller/products/page.js
+++ src/app/(sellerLayout)/seller/products/page.js
@@ -1,6 +1,10 @@
+import StatCards from '@/components/sellerLayout/dashboardPage/StatCards';
 import PageHeaderSetter from '@/components/sellerLayout/shared/PageHeaderSetter';
 import { sellerDashboardPageMeta } from '@/lib/sellerDashboardPageMeta';
 import React from 'react';
+import { AiFillProduct } from 'react-icons/ai';
+import { FaCircleCheck, FaTriangleExclamation } from 'react-icons/fa6';
+import { PiChartLineDownBold } from 'react-icons/pi';
 
 export const metadata = sellerDashboardPageMeta.products.metadata
 
@@ -11,9 +15,55 @@ const SellerProductsPage = () => {
         heading={sellerDashboardPageMeta.products.heading}
         subheading={sellerDashboardPageMeta.products.subheading}
       />
-      Seller All Products will go here with all details.
+
+      {/* STAT CARDS */}
+      <StatCards statCardData={statCardData} />
     </React.Fragment>
   );
 };
 
 export default SellerProductsPage;
+
+
+// STAT CARD's DATA
+const statCardData = [
+  {
+      id:1,
+      label:"Total Products",
+      value:"40",
+      change:"14.8%",
+      trend:"up",
+      intent:"info",
+      icon: <AiFillProduct />,
+    },
+
+    {
+      id:2,
+      label:"Active Products",
+      value:"34",
+      change:"9.2%",
+      trend:"up",
+      intent:"accent",
+      icon: <FaCircleCheck />,
+    },
+
+    {
+      id:3,
+      label:"Out of Stock",
+      value:"5",
+      change:"1.15%",
+      trend:"down",
+      intent:"danger",
+      icon: <FaTriangleExclamation />,
+    },
+
+    {
+      id:4,
+      label:"Low Stock (<5)",
+      value:"1",
+      change:"0.0%",
+      trend:"neutral",
+      intent:"waning",
+      icon: <PiChartLineDownBold />,
+    }
+]
