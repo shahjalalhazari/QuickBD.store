202f21cf4910548c003b57a9d7acca94bb3351a8
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2026-05-28 14:55:34 +0400

    Some changes on dashboard page's chart.

--- src/components/sellerLayout/dashboardPage/SalesOverviewChart.jsx
+++ src/components/sellerLayout/dashboardPage/SalesOverviewChart.jsx
@@ -1,65 +1,21 @@
 "use client"
-import OutlineDropdown from '@/components/shared/filters/OutlineDropdown';
-import { useMemo, useState } from "react";
+import Link from 'next/link';
 import MetricPillCard from '../shared/cards/MetricPillCard';
 import LineChart from '@/components/shared/charts/LineChart';
+import ViewAllBtn from '@/components/shared/buttons/ViewAllBtn';
 
 
+// DEMO DATA
 const analyticsData = {
-  "7d": {
-    label: "Last 7 Days",
-    labels:["Mon","Tue","Wed","Thu","Fri","Sat","Sun"],
-    sales:[620,740,680,390,940,1080,1190],
-    orders:[18,22,20,28,31,34,39]
-  },
-
-  "30d": {
-    label: "Last 30 Days",
-    labels:["01","03","05","07","09","11","13","15","17","19","21","23","25","27","29"],
-    sales:[4200,5100,4800,6400,1234,2345,3456,4567,5678,6789,7890,5624,9876,7840,7564],
-    orders:[145,172,161,210,123,234,345,456,567,678,789,890,987,876,765]
-  },
-
-  "90d": {
-    label: "Last 90 Days",
-    labels:["Jan","Feb","Mar"],
-    sales:[12400,15100,16800],
-    orders:[452,508,571]
-  },
-
-  "6m": {
-    label: "Last 6 Months",
-    labels:["Jan","Feb","Mar","Apr","May","Jun"],
-    sales:[4200,5100,4800,6400,7200,8100],
-    orders:[145,172,161,210,248,279]
-  },
-
-  "thisYear": {
-    label: "This Year",
-    labels:["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dev"],
-    sales:[4200,5100,4800,6400,7200,8100,8900,10000,7920,8800,11002,7050],
-    orders:[145,172,161,210,248,279,305,500,320,439,893,123]
-  },
+  label: "Last 7 Days",
+  labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+  sales: [620, 740, 680, 390, 940, 1080, 1190],
+  orders: [18, 22, 20, 28, 31, 34, 39],
 };
 
-const filters = [
-  {value:"7d",label:"Last 7 Days"},
-  {value:"30d",label:"Last 30 Days"},
-  {value:"90d",label:"Last 90 Days"},
-  {value:"6m",label:"Last 6 Months"},
-  {value:"thisYear",label:"This Year"},
-];
-
 const SalesOverviewChart = () => {
-  const [range, setRange] = useState(filters[0]);
-
-  const selectedData = useMemo(
-    () => analyticsData[range.value],
-    [range]
-  );
-
-  const currentSales=selectedData.sales[selectedData.sales.length-1];
-  const currentOrders= selectedData.orders[selectedData.orders.length-1];
+  const currentSales = analyticsData.sales[analyticsData.sales.length - 1];
+  const currentOrders = analyticsData.orders[analyticsData.orders.length - 1];
 
   const metricPillData = [
     {
@@ -90,16 +46,17 @@ const SalesOverviewChart = () => {
       {/* HEADER AND DROPDOWN */}
       <div className="chart-header">
         <div className='space-y-1 md:space-y-2'>
-          <p className="chart-sub-heading">Performance Analytics</p>
+          <p className="chart-sub-heading">This Week</p>
           <h2 className="chart-heading">Sales Overview</h2>
         </div>
 
         {/* FILTER DROPDOWN */}
-        <OutlineDropdown
-          options={filters}
-          value={range}
-          onChange={setRange}
-        />
+        <Link href={"/seller/analytics"}>
+          <ViewAllBtn
+            text={"See More"}
+            className={"view-all-btn"}
+          />
+        </Link>
       </div>
 
       {/* METRIC PILLS */}
@@ -110,7 +67,7 @@ const SalesOverviewChart = () => {
       </div>
 
       {/* CHART */}
-      <LineChart selectedData={selectedData} />
+      <LineChart selectedData={analyticsData} />
     </section>
   );
 };
